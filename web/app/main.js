455fb7069e8fc772cf2a3415a49fd640118fc68a
Emilian Patrasc <emilian.patrasc@edu.unito.it>
2021-10-30 19:06:07 +0200

    complete app test

--- web/app/main.js
+++ web/app/main.js
@@ -1,12 +1,27 @@
-angular
-    .module("ohm-delivery", [])
-    .controller("tracking", function($scope, $http) {
-        $scope.sendData = function() {
-            $http.get(`/ohms/${this.trackingId}`)
-            .then((error) => {
-                this.errorMessage = 'Oops, this website is under construction, please come back later.';
-            }, (result) => {
-                this.errorMessage = '';
-            });
-        };
+const fetchOhm = ($http, $context) => {
+  $http.get(`/ohms/${$context.trackingId}`)
+    .then((result) => {
+      $context.ohm = result.data;
+      $context.errorMessage = undefined;
+    }, (error) => {
+      $context.ohm = undefined;
+      $context.errorMessage = error.data;
     });
+}
+angular
+  .module("ohm-delivery", [])
+  .controller("tracking", function ($scope, $http) {
+    $scope.sendData = function () {
+      return fetchOhm($http, this)
+    };
+
+    $scope.onedit = function () {
+      if (!this.trackingId) {
+        this.ohm = undefined;
+        this.errorMessage = undefined;
+        return;
+      };
+
+      return fetchOhm($http, this)
+    }
+  });
